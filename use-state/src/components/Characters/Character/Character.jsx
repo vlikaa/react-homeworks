5332de4644a0e55f6a60f700194da4baecc7f6d7
Ali Veliyev <aliveliyev123654@gmail.com>
2024-12-11 19:41:27 +0400

    added Characters & Character & Pagination components

---
+++ use-state/src/components/Characters/Character/Character.jsx
@@ -0,0 +1,10 @@
+﻿const Character = ({characterData}) => {
+	return (
+		<div className="character">
+			<img src={characterData.image} alt="character-img"/>
+			<span>{characterData.name}</span>
+		</div>
+	);
+}
+
+export default Character;
---
+++ use-state/src/components/Characters/Characters.jsx
@@ -0,0 +1,17 @@
+﻿import Character from "./Character/Character";
+
+const Characters = ({charactersData}) => {
+	return (
+		<ul className="characters">
+			{
+				charactersData.map(character => (
+					<li>
+						<Character characterData={character} />
+					</li>
+				))
+			}
+		</ul>
+	);
+}
+
+export default Characters;
